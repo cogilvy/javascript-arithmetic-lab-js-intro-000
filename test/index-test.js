9c7072757188be60a20efa9f237846a810de87a2
diligent-utility-1811 <chrisogilvy@cox.net>
2019-02-10 00:09:45 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -30,19 +30,7 @@ it('dec(n) decrements n and returns the result', function() {
 })
 
 describe('makeInt(n)', function() {
-  it('describe('makeInt(n)', function() {
   it('parses n as an integer and returns the parsed integer', function() {
-    expect(makeInt(a.toString())).toEqual(a)
-  })
-
-  it('assumes base 10', function() {
-    expect(makeInt('0x2328')).toEqual(0)
-  })
-
-  it('returns NaN as appropriate', function() {
-    expect(isNaN(makeInt('sldkjflksjf'))).toEqual(true)
-  })
-})', function() {
     expect(makeInt(a.toString())).toEqual(a)
   })
 
